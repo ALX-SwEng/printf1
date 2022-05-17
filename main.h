ab4d4166b8ab48e802c69ac67e9fa26968570b1b
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-18 00:56:35 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,5 +1,6 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,6 +12,5 @@
 void _putchar(char);
 int _printf(const char *format, ...);
 int print_output(const char *format, va_list val, char* buffer);
-
 
 #endif
