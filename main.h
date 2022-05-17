a8dce90fe9263858cab7178819d19ea22a3699af
“horluwaluvme” <pymfemi@gmail.com>
2022-05-17 12:53:15 -0700

    main.h

--- main.h
+++ main.h
@@ -1,6 +1,12 @@
 #ifndef _MAIN_H_
-
 #define _MAIN_H_
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <ctype.h>
+
 
 void _putchar(char);
 int _printf(const char *format, ...);
