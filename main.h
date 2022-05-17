8d230f27c3848a631cd2f7863712b1350bcdc031
horluwaluvme <91141140+horluwaluvme@users.noreply.github.com>
2022-05-18 01:47:15 +0200

    Update main.h

--- main.h
+++ main.h
@@ -1,16 +1,10 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-#include <limits.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <string.h>
-#include <ctype.h>
-
-
 void _putchar(char);
 int _printf(const char *format, ...);
 int print_output(const char *format, va_list val, char* buffer);
+int print(const char *format, va_list val, char* buffer);
+int printf_int(va_list arg);
 
 #endif
