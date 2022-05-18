a3727169e889b2e7d107f03d8a29dcdd847b5e27
“horluwaluvme” <pymfemi@gmail.com>
2022-05-17 17:44:32 -0700

    main.h

--- main.h
+++ main.h
@@ -5,5 +5,6 @@ void _putchar(char);
 int _printf(const char *format, ...);
 int printf_output(const char *format, va_list val, char *buffer);
 int printf_int(va_list arg);
+int printf_hexa(va_list arg);
 
 #endif
