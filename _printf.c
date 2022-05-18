2fb34e2d081bd6127676f6a2680f99c53f1dc52c
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-18 04:09:56 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -14,19 +14,17 @@ int _printf(const char *format, ...)
 	int counter;
 
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);				
+		return (-1);
 	va_start(varAargs, format);
 	counter = printf_output(format, varAargs);
-	va_end(varAargs);		
+	va_end(varAargs);
 return (counter);
 }
 
 /**
  * print_output - print a formatted output.
- *
  * @format: string pointer to print - may contain directives.
  * @val: Variable number of arguments.
- *
  * Return: # of characters printed.
  */
 
@@ -34,6 +32,7 @@ int printf_output(const char *format, va_list val)
 {
 	int i = 0, j = 0, k = 0;
 	char *tmps, tmpd[20], c;
+
 	while (format[i])
 	{
 		if (format[i] == '%')
@@ -48,33 +47,32 @@ int printf_output(const char *format, va_list val)
 				_putchar(format[i]);
 				j++;
 				return (j); }
-			else
-			{
-				i++;
-		 		switch (format[i]) 
-		 		{
-				case 'c':
-					c = (char)va_arg(val, int);
-					_putchar(c);
-					j++; 
-					break;
-				case 's':
-					tmps = va_arg(val, char*);
-					if (tmps == NULL)
-						return (NULL);
-					while(tmps[k])
-					{
-						_putchar(tmps[k++]);
-						j++; }
-					break;
-			 	case 'd':
-				case 'i':
-					j += printf_int(val);
-					break;
-				default:
-					_putchar(format[i]);
-					++j;
-		        } } }
+
+			i++;
+	 		switch (format[i]) 
+	 		{
+			case 'c':
+				c = (char)va_arg(val, int);
+				_putchar(c);
+				j++; 
+				break;
+			case 's':
+				tmps = va_arg(val, char*);
+				if (tmps == NULL)
+					return (NULL);
+				while(tmps[k])
+				{
+					_putchar(tmps[k++]);
+					j++; }
+				break;
+		 	case 'd':
+			case 'i':
+				j += printf_int(val);
+				break;
+			default:
+				_putchar(format[i]);
+				++j;
+	        } } }
 	    else
 	    {
 		  _putchar(format[i]);
