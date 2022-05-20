89a6f7d2589c6d08eb24a46eeee5a76975731a37
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-21 01:11:42 +0300

    0x11. C - printf

--- _printf.c
+++ _printf.c
@@ -48,7 +48,10 @@ int printf_output(const char *format, va_list val)
 				if (f == NULL)
 				{
 					if (format[i] != '%')
+					{
 						_putchar(format[i - 1]);
+						++counter;
+					}
 					_putchar(format[i]);
 					++counter;
 				}
