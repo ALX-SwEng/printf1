e492b97dec9f2c4beef43a45de0d4ece1f9b25b2
Chere Lemma <chere.lemma@aastu.edu.et>
2022-05-21 21:25:18 +0300

    Update base.c

--- base.c
+++ base.c
@@ -24,28 +24,28 @@ unsigned char flags, int wid, int prec)
 	int size;
 	char digit, pad = '0';
 	unsigned int ret = 1;
-	
+
 	for (size = 0; *(base + size);)
 		size++;
-		
+
 	if (num >= size || num <= -size)
 		ret += convert_sbase(output, num / size, base, flags, wid - 1, prec - 1);
 	else
 	{
 		for (; prec > 1; prec--, wid--) /* Handle precision */
 		ret += _memcpy(output, &pad, 1);
-		
+
 		if (NEG_FLAG == 0) /* Handle width */
 		{
 			pad = (ZERO_FLAG == 1) ? '0' : ' ';
 			for (; wid > 1; wid--)
-			ret += _memcpy(output, &pad, 1); 
+			ret += _memcpy(output, &pad, 1);
 			}
 		}
 
 		digit = base[(num < 0 ? -1 : 1) * (num % size)];
 		_memcpy(output, &digit, 1);
-	return (ret); 
+	return (ret);
 }
 
 /**
@@ -88,6 +88,5 @@ unsigned char flags, int wid, int prec)
 			ret += _memcpy(output, lead, 2); }
 	digit = base[(num % size)];
 	_memcpy(output, &digit, 1);
-return (ret); 
+return (ret);
 }
-
